c9e684f155828b270596a4dd9a47bfabdcdcbf5c
Singh <d.aj.singh@accenture.com>
2025-07-08 23:23:04 +0530

    Committing Day 1 of 30 days of reaact

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from "./components/Counter"
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{textAlign:"center"}}>React Day 1 to 30</h1>
+      <div>
+        <h2 style={{textAlign:"center"}}>Project 1 - Counter APP</h2>
+        <Counter />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './styles/counter.css'
+const Counter = () => {
+  const [counter, setCounter] = useState(0);  
+
+  const incrementCounter = () => {
+    setCounter((prevCount) => prevCount + 1);
+  }
+
+  const decrementCounter = () => {
+    setCounter((prevCount) => {
+        if(prevCount == 0){
+            return 0
+        }
+        else{
+            return prevCount - 1;
+        }
+    });
+  }
+
+  const resetCounter = () => {
+    setCounter(0);
+  }
+  return (
+    <div style={{textAlign:"center"}}>
+      <div className='counterDiv'>{counter}</div>
+      <div className='counterButton'>
+        <button className='circle' onClick={incrementCounter}>+</button>
+        <button className='oval' onClick={resetCounter}>Reset Counter</button>
+        <button className='circle' onClick={decrementCounter}>-</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
